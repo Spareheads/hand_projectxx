94d3330b3bfcad5ade6f081566f056caf0d67340
Spareheads <94579408+Spareheads@users.noreply.github.com>
2022-06-13 17:54:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+    });
+    
+    camera = document.getElementById("camera");
+    Webcam.attach ( '#camera' );
+    
+    
+    
+    function take_snapshot()
+    {
+    Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    });
+    }
+    
+    
+    
+    
+    console.log('m15 version', m15.version);
+    classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/SBJGZrvwm/model.json', modeLoaded);
+    
+    
+    function modelLoaded() {
+    
+    console.log('modelLoaded!');
+    }
+    
+    function speak() {
+    
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is" + prediction_1;
+    speak_data_2 = "And the second prediction is" + prediction_2;
+    var utterThis = new SpeechSynthesisisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+    }
+
+    function check()
+    {
+      img = document.getElementById('captured_image');
+      classifier.classify(img, gotResult);
+      
+    }
+
+
+    function gotResult(error, results) {
+    if(error) {
+    console.error(error);
+    } else {
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML = results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if(results[0].label == "ok")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#128512;"
+    }
+
+
+   } 
+   }
+   
